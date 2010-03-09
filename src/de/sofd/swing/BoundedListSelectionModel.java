9ba1f52401b0f86d081783a89f05effc772488bc
Olaf Klischat <olaf.klischat@sofd.de>
2010-03-09 15:45:52 +0000

    BoundedListSelectionModel + default impl. factored out of test app

---
+++ src/de/sofd/swing/BoundedListSelectionModel.java
@@ -0,0 +1,25 @@
+package de.sofd.swing;
+
+import javax.swing.ListSelectionModel;
+
+public interface BoundedListSelectionModel extends ListSelectionModel {
+
+    public abstract int getLowerBound();
+
+    public abstract void setLowerBound(int lowerBound);
+
+    public abstract void disableLowerBound();
+
+    public abstract int getUpperBound();
+
+    public abstract void setUpperBound(int upperBound);
+
+    public abstract void disableUpperBound();
+
+    public abstract int[] getBounds();
+
+    public abstract void setBounds(int lower, int upper);
+
+    public abstract void disableBounds();
+
+}
